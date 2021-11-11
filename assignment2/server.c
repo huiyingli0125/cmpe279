a77e59f4b27a90cce8acf8dcf6485c6b8e846cab
charliewei6 <xichao.wei@sjsu.edu>
2021-11-11 15:43:30 -0800

    assignment2 finished

---
+++ assignment2/server.c
@@ -0,0 +1,95 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pwd.h>
+#define PORT 8080
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+    int status;
+
+    printf("re-exec the child process\n");
+    if (argc == 1) {
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+
+    // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+    // &opt, sizeof(opt)))
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+        &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons( PORT );
+
+        if (bind(server_fd, (struct sockaddr *)&address,
+        sizeof(address)) < 0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                        (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        //in child process
+        pid_t childPid = fork();
+        if (childPid == 0) {
+            uid_t nobodyUid = getpwnam("nobody")->pw_uid;
+            printf("nobody uid id: %ld\n", (long) nobodyUid);
+            setuid(nobodyUid);
+            
+            char args_sock[100];
+            sprintf(args_sock, "%d", new_socket);
+            char args_original[100];
+            sprintf(args_original, "%s", argv[0]);
+            char *args[] = {args_original, args_sock, NULL};
+            execvp(args[0], args);  
+
+        } else if (childPid > 0) {
+            if (wait(&status) == -1) {
+                printf("Parrent on waiting error\n");
+            }
+            printf("Child process exited\n");
+            exit(0);
+        } else {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+    }else{
+        new_socket = atoi(argv[1]);
+        valread = read(new_socket, buffer, 1024);
+        printf("Read %d bytes: %s\n", valread, buffer);
+        send(new_socket, hello, strlen(hello), 0);
+        printf("Hello message sent\n");
+    }
+
+    return 0;
+}
